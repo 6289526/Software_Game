9736de749fcb2b0a0793cf0d3e3ede10ce2ad915
そな <sona1125kojiman@icloud.com>
2020-08-10 16:27:08 +0900

    クライアント用のmain.cppを作成

---
+++ Game/client/header/client_common.h
@@ -0,0 +1,14 @@
+/*
+ *  ファイル名  : client_common.h
+ *  機能    : クライアント用のマクロ、構造体等の定義
+ */
+
+#ifndef CLIENT_COMMON_H
+#define CLIENT_COMMON_H
+
+/*----------include 開始----------*/
+#include"../../header/constants.h"
+
+/*----------include 終了----------*/
+
+#endif
