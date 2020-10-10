3d20ef8cba71c0de67f425fd15fb509fd0340732
Nameless <tanisyun0331@icloud.com>
2020-10-10 16:42:36 +0900

    edit map.hpp

--- Game/header/map.hpp
+++ Game/header/map.hpp
@@ -1,32 +1,35 @@
 #pragma once
 
 #include "constants.h"
+#include <vector>
+
+using namespace std;
 
 #define MAP_SIZE_W 20
 #define MAP_SIZE_H 20
+#define MAP_SIZE_D 20
 #define OBJECT_NUM 5
 
 class MapData{
     protected:
-        int _TerrainData[MAP_SIZE_W][MAP_SIZE_H] = {0};
-        PlaceData _ObjectDatas[OBJECT_NUM];
-        int _MapW = MAP_SIZE_W, _MapH = MAP_SIZE_H;
-        int _ObjectNum = 0;
+        int _TerrainData[MAP_SIZE_W][MAP_SIZE_H][MAP_SIZE_D] = {0};
+        vector<PlaceData> _ObjectDatas;
+        int _MapW = MAP_SIZE_W, _MapH = MAP_SIZE_H, _MapD = MAP_SIZE_D;
 
     public:
-        const int(* GetTerrainData())[MAP_SIZE_H]{
+        const int(* GetTerrainData())[MAP_SIZE_H][MAP_SIZE_D]{
             return _TerrainData;
-        };
-        const PlaceData* GetObjectData(){
+        }
+        const vector<PlaceData> GetObjectData(){
             return _ObjectDatas;
-        };
+        }
         int GetMapW(){
             return _MapW;
         }
         int GetMapH(){
             return _MapH;
         }
-        int GetObjectNum(){
-            return _ObjectNum;
+        void PushBackObject(PlaceData *objectData){
+            _ObjectDatas.push_back(*objectData);
         }
 };
