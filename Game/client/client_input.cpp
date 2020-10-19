8109f095a0c4d26fb9914ff418fc758b22f0473c
sona <lite01250@gmail.com>
2020-10-19 18:29:27 +0900

    Input関係のコメントを修正

--- Game/client/client_input.cpp
+++ Game/client/client_input.cpp
@@ -1,10 +1,12 @@
 #include "client_input.h"
-InputModuleBase::InputModuleBase(){   
+InputModuleBase::InputModuleBase(){
 }
+
 InputModuleBase::~InputModuleBase(){
-    // printf("siieeeeeeee");    
+    // printf("siieeeeeeee");
 }
+
+// 現在の入力情報を返す
 InputType InputModuleBase::GetInputType(){
-    // 現在の入力情報を返す
     return _Input;
 }
