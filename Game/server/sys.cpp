c6361c73a4af048a3aa9247c292e5a5a3597875f
6289526 <hakase6289@ezweb.ne.jp>
2020-11-04 17:55:55 +0900

    cmake, csvファイル変更

--- Game/server/sys.cpp
+++ Game/server/sys.cpp
@@ -27,7 +27,7 @@ const PlayerData *GetPlayerData()
 
 void InitSys(char* file) // システム初期化
 {
-    Map.LoadMapData(file);
+    //Map.LoadMapData(file);
 }
 
 BlockType Collision_CB(int chara_ID, int y, int accuracy)
@@ -252,7 +252,7 @@ void PutBlock(int chara_ID) // ブロックを置けるなら置く
     // ブロックを置けるなら
     if (Collision_BB()) {
         // マップに追加
-        Map.PushBackObject(&PlData);
+        Map.SetObjectData(&PlData);
         // クライアント全員に送信
         RunCommand(BROADCAST, PUT_COMMAND);
     }
