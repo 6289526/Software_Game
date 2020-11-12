f7db6ca56ab3379aeb2d2864bdff35f47d8c88c1
6289526 <hakase6289@ezweb.ne.jp>
2020-11-12 23:01:39 +0900

    横の当たり判定の強化

--- Game/client/client_system.cpp
+++ Game/client/client_system.cpp
@@ -2,7 +2,7 @@
 #include "graphic.h"
 #include <string.h>
 
-#define PLAYER_MOVE_SPEED 15
+#define PLAYER_MOVE_SPEED 40
 #define GRAVITY 9.8 * 0.5// * 3
 
 static int MyId; // クライアントのID
@@ -253,7 +253,7 @@ bool IsPlayerOnGround(){
     int Block_X = 0;
     int t_Block_Y = (PData[id].pos.y + PData[id].velocity.y + y);
     int Block_Y = (PData[id].pos.y + PData[id].velocity.y + y) / MAP_MAGNIFICATION;
-    Block_Y = clamp(Block_Y, 0, MAP_SIZE_H - 1); 
+    Block_Y = clamp(Block_Y, 0, MAP_SIZE_H - 1);
 	int Block_Z = 0;
 
     for (int i = 0; i < accuracy; ++i)
