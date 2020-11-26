d442ed10f226abf39778e039244af9de6061b7bb
sona <lite01250@gmail.com>
2020-11-26 18:10:21 +0900

    一時保存

--- Game/client/client_gamestate.cpp
+++ Game/client/client_gamestate.cpp
@@ -1,11 +1,35 @@
 #include "client_common.h"
 
-GameStateController::GameStateController(){
 
+// ===== * ===== EventHandler ===== * ===== //
+void TestFunc(GameState state) {
+    fprintf(stderr, "state type: %d\n", state);
+}
+
+void GameStateEventHandler::BindEvent(GameStateEventSource *source){
+    __hook(&GameStateEventSource::OnChangeGameState, source, TestFunc);
+}
+
+void GameStateEventHandler::RemoveEvent(GameStateEventSource *source){
+    __unhook(&GameStateEventSource::OnChangeGameState, source, TestFunc);
+}
+
+GameStateEventHandler::GameStateEventHandler(GameStateEventSource *source){
+    BindEvent(source);
+}
+
+GameStateEventHandler::~GameStateEventHandler(){
+    RemoveEvent(_Source);
+}
+// ===== * ===== EventController ===== * ===== //
+GameStateController::GameStateController(){
+    _Source = new GameStateEventSource();
+    _Handler = new GameStateEventHandler(_Source);
 }
 
 GameStateController::~GameStateController(){
-
+    delete _Handler;
+    delete _Source;
 }
 
 GameState GameStateController::GetGameState(){
--- Game/client/client_gamestate.hpp
+++ Game/client/client_gamestate.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "stdio.h"
+#include <event.h>
 
 enum GameState{ 
     Init = 0,
@@ -9,10 +9,35 @@ enum GameState{
     EnterPlayerName,
 };
 
+// Eventの発行物を定義するクラス
+class GameStateEventSource{
+public:
+    // ゲームステート変更時に発行するイベント
+    event void OnChangeGameState(GameState state);
+
+    GameStateEventSource();
+    ~GameStateEventSource();
+};
+
+// Eventの購読を行うクラス
+class GameStateEventHandler{
+private:
+    GameStateEventSource *_Source;
+
+public:
+    GameStateEventHandler(GameStateEventSource *source);
+    ~GameStateEventHandler();
+    void BindEvent(GameStateEventSource *source);
+    void RemoveEvent(GameStateEventSource *source);
+};
+
+// Eventの発行を行うクラス
 class GameStateController
 {
 private:
     GameState _State = GameState::Init;
+    GameStateEventSource *_Source;
+    GameStateEventHandler *_Hander;
 
 public:
     GameStateController();
