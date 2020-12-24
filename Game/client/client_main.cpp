d8667ac0c43781240f651586eac8061217a0c30f
qriquri <qriputo910@gmail.com>
2020-12-24 22:37:54 +0900

    戻した

--- Game/client/client_main.cpp
+++ Game/client/client_main.cpp
@@ -29,7 +29,13 @@ int main(int argc, char *argv[])
 	u_short port = DEFAULT_PORT;
 
 	char server_name[MAX_LEN_NAME];
-	
+	//multithread
+	SDL_Thread *SelectThread;
+	SDL_mutex *mtx1 = SDL_CreateMutex();
+	SelectThread = SDL_CreateThread(Select, "getCommand", mtx1);
+	SDL_Thread *goThread;
+	SDL_mutex *gMtx = SDL_CreateMutex();
+	goThread = SDL_CreateThread(Go, "Go!", gMtx);
 	/*初期設定*/
 	sprintf(server_name, "localhost");
 
@@ -66,14 +72,6 @@ int main(int argc, char *argv[])
 	/**サーバー関連 END**/
 	_System.InitSystem(_System.GetInitData());
 	Init2dGraphic();
-
-	//multithread
-	SDL_Thread *SelectThread;
-	SDL_mutex *mtx1 = SDL_CreateMutex();
-	SelectThread = SDL_CreateThread(Select, "getCommand", mtx1);
-	SDL_Thread *goThread;
-	SDL_mutex *gMtx = SDL_CreateMutex();
-	goThread = SDL_CreateThread(Go, "Go!", gMtx);
 
 	while (cond && !_System.GetInitData()->input->GetInputType().End)
 	{
